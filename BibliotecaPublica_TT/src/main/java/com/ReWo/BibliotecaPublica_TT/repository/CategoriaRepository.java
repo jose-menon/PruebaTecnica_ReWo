ee9b10eec81117b25f9f50d8e91121e4deaa05ad
Jose Menon <menon.jose@outlook.com>
2026-04-08 19:49:03 -0300

    se integra la capa de comunicacion entre la DB y la logica de negocio

---
+++ BibliotecaPublica_TT/src/main/java/com/ReWo/BibliotecaPublica_TT/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.ReWo.BibliotecaPublica_TT.repository;
+
+import com.ReWo.BibliotecaPublica_TT.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>
+{
+    Optional<Categoria> findByNombre(String nombre);
+
+    boolean existsByNombre(String nombre);
+}
---
+++ BibliotecaPublica_TT/src/main/java/com/ReWo/BibliotecaPublica_TT/repository/LibroRepository.java
@@ -0,0 +1,38 @@
+package com.ReWo.BibliotecaPublica_TT.repository;
+
+import com.ReWo.BibliotecaPublica_TT.entity.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LibroRepository extends JpaRepository<Libro, Long>
+{
+    Optional<Libro> findByIsbn(String isbn);
+
+    boolean existsByIsbn(String isbn);
+
+    List<Libro> findByTituloContainingIgnoreCase(String titutlo);
+
+    List<Libro> findByAutorContainingIgnoreCase(String autor);
+
+    List<Libro> findByCategoriaNombreIgnoreCase(String nombreCategoria);
+
+    List<Libro> findByEjemplaresDisponiblesGreaterThan(Integer cantidad);
+
+    List<Libro> findByEjemplaresDisponiblesEquals(Integer cantidad);
+
+    List<Libro> findByTituloContainingIgnoreCaseAndCategoriaNombreIgnoreCase(String titulo, String categoria);
+
+    @Query("""
+        SELECT l FROM Libro l WHERE LOWER(l.titulo) LIKE LOWER(CONCAT('%', :texto, '%'))
+        OR LOWER(l.autor) LIKE LOWER(CONCAT('%', :texto, '%'))
+        OR LOWER(l.isbn) LIKE LOWER(CONCAT('%', :texto, '%'))
+""")
+    List<Libro> buscarEnCatalogo(String texto);
+    @Query("""
+        SELECT l FROM Libro l WHERE l.ejemplaresDisponibles = 0
+""")
+    List<Libro> obtenerLibrosAgotados();
+}
