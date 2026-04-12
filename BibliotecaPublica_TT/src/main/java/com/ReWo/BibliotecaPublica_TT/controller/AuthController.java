a7a5a5649eaddbbd36afc6a76d6635b7491c200d
Jose Menon <123137667+jose-menon@users.noreply.github.com>
2026-04-12 18:16:37 -0300

    Fix API request mapping for AuthController

--- BibliotecaPublica_TT/src/main/java/com/ReWo/BibliotecaPublica_TT/controller/AuthController.java
+++ BibliotecaPublica_TT/src/main/java/com/ReWo/BibliotecaPublica_TT/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("api//auth")
 @CrossOrigin(origins = "http://localhost:5173")
 public class AuthController
 {
